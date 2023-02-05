0c808b117f6e3d3a65cff645564d9124d5b2df2c
VadimZP <vadymchernykh@gmail.com>
2023-02-05 03:18:31 +0200

    Finished Gatsby Tutorial Part 2

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,14 +2,14 @@ import * as React from "react"
 import { HeadFC, Link } from "gatsby"
 import Layout from "../components/layout"
 
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
 }
 
 export const Head: HeadFC = () => <title>Home Page</title>
 
-export default IndexPage
+export default AboutPage
